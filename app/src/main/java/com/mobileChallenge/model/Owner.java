089a4e59540d0d214c323c747c9f04a09aa56559
Th3CracKed <abdodriowya@gmail.com>
2018-11-20 22:26:27 +0100

    Fix memory leak by adding config to activity
    - to prevent runtime restart due to orientation change android:configChanges="orientation|screenSize"

--- app/src/main/java/com/mobileChallenge/model/Owner.java
+++ app/src/main/java/com/mobileChallenge/model/Owner.java
@@ -3,7 +3,6 @@ package com.mobileChallenge.model;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.mobileChallenge.R;
 
 import androidx.databinding.BindingAdapter;
 
@@ -23,24 +22,15 @@ public class Owner {
         return avatar_url;
     }
 
-    /** TODO fix Memory leak when turning the screen :  A solution will be to use getApplicationContext() but it's not recommended i think
+    /**
      * used by data binding library to fetch image
      * @param imageView the imageView that will be be binded
      * @param imageUrl the url to fetch
      */
     @BindingAdapter("imageUrl")
     public static void loadImage(ImageView imageView, String imageUrl) {
-        if (imageUrl != null) {
-            // If we don't do this, you'll see the old image appear briefly
-            // before it's replaced with the current image
-            if (imageView.getTag(R.id.avatarImage) == null || !imageView.getTag(R.id.avatarImage).equals(imageUrl)) {
-                imageView.setImageBitmap(null);
-                imageView.setTag(R.id.avatarImage, imageUrl);
-                Glide.with(imageView.getContext()).load(imageUrl).into(imageView);
-            }
-        } else {
-            imageView.setTag(R.id.avatarImage, null);
-            imageView.setImageBitmap(null);
+        if (imageView != null) {
+            Glide.with(imageView.getContext()).load(imageUrl).into(imageView);
         }
     }
 }
